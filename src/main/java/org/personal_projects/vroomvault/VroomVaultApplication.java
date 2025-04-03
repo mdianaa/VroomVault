a7d47a7bfa30b2fbd2e2c47365a6417b7ef51185
Diana Mihaylova <dmihaylova@dmihaylova-mac.lo.devexperts.com>
2025-04-03 20:13:13 +0300

    initial commit for project

---
+++ src/main/java/org/personal_projects/vroomvault/VroomVaultApplication.java
@@ -0,0 +1,13 @@
+package org.personal_projects.vroomvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VroomVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/personal_projects/vroomvault/VroomVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal_projects.vroomvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
